bf041f77364be070b976cb4279007e09b234a8e2
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-05-11 23:40:57 +0100

    hello

--- backend/AgroApi/admin.py
+++ backend/AgroApi/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile, UserInterest, UserEducation, UserLocation, PostComment, PostLike, Post, GroupMember, Group, Message, MetaTags
+
+all_models = [UserLocation, UserEducation, UserInterest, UserProfile, PostComment, PostLike, Post, GroupMember, Group, Message, MetaTags]
+for model in all_models:
+    admin.site.register(model)
+
 
 # Register your models here.
+
