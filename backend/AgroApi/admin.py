3d83564e9d10d2b8b406d13256f07ad41a0a58b3
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2020-05-12 04:52:31 +0100

    Tested the migrations

--- backend/AgroApi/admin.py
+++ backend/AgroApi/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, UserInterest, UserEducation, UserLocation, PostComment, PostLike, Post, GroupMember, Group, Message, MetaTag
+from .models import UserProfile, UserInterest, UserEducation, PostComment, PostLike, Post, GroupMember, Group, MetaTag
 
-all_models = [UserLocation, UserEducation, UserInterest, UserProfile, PostComment, PostLike, Post, GroupMember, Group, Message, MetaTag]
+all_models = [UserEducation, UserInterest, UserProfile, PostComment, PostLike, Post, GroupMember, Group, MetaTag]
 for model in all_models:
     admin.site.register(model)
 
