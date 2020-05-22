037c17566af434b13d510cb0c61dda007b395855
Mazeli Victor <victor.vic.mazeli@gmail.com>
2020-05-22 01:48:23 +0100

    url settings updated

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import settings
-from AgroApi import views
+from . import views
 from django.conf.urls.static import static
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
